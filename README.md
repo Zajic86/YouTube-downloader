5a59c6417ea120650ca5658d3ba30684f4ae25fb
Zajic86 <info@martinlukasek.cz>
2024-04-13 23:08:33 +0200

    Make v1.0

--- README.md
+++ README.md
@@ -4,17 +4,24 @@ A simple terminal downloader that allows you to download a video or just an audi
 ## Libraries used
 - moviePy
 - pyTube
+- colorama
 
 ```bash
-pip install pytube moviepy
+pip install pytube moviepy colorama
 ```
 
 ## Installation
 
 ```bash
 git clone https://github.com/Zajic86/YouTube-downloader.git
-cd YouTube-downloader/yt-down
+cd YouTube-downloader
+pip install -r requirements.txt
+cd yt-down
 python yt-down.py
 ```
-or just copy/paste code from yt-down/yt-down.py
+or just copy/paste code from yt-down/yt-down.py and install libraries.
 
+If you have a problem with decoding to mp3 (Ubuntu) try install mpg123:
+```bash
+sudo apt install mpg123
+```
--- yt-down/yt-down.py
+++ yt-down/yt-down.py
@@ -2,6 +2,7 @@
 
 from pytube import YouTube
 from moviepy.editor import *
+from colorama import Fore
 
 
 def download_audio_highest_quality(in_url, output_path):
@@ -12,7 +13,7 @@ def download_audio_highest_quality(in_url, output_path):
     audio_streams = video_obj.streams.filter(only_audio=True).order_by('abr').desc()
     highest_quality_audio = audio_streams.first()
     audio_file_path = highest_quality_audio.download(output_path=output_path)
-    print("Audio track (music) has been downloaded to:", audio_file_path)
+    print(Fore.BLUE + "OK - Audio downloaded:", audio_file_path + Fore.RED)
     convert_to_mp3(audio_file_path)
 
 
@@ -23,30 +24,30 @@ def convert_to_mp3(input_path):
     audio = AudioFileClip(input_path)
     output_path = input_path[:-5] + ".mp3"
     audio.write_audiofile(output_path)
-    print("Audio track has been converted to MP3 format:", output_path)
+    print(Fore.BLUE + "OK - Audio converted & saved to mp3:", output_path)
     os.remove(input_path)  # Delete the original audio file
 
 
 if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.realpath(__file__))
     while True:
-        input_url = input("Enter the YouTube video URL: ")
+        input_url = input(Fore.GREEN + "Enter the YouTube video URL: ")
         if "youtube" in input_url:
-            choice = input("Download complete video (1) or just the audio (2)? Enter 1 or 2: ")
-            print("Processing...")
+            choice = input("Complete video (1) or just the audio (2)? Enter 1 or 2: ")
+            print(Fore.RED + "Processing...")
             if choice == "1":
                 video = YouTube(input_url)
                 stream = video.streams.get_highest_resolution()
                 save_path = os.path.join(script_dir, "video")
                 stream.download(output_path=save_path)
-                print("Video has been downloaded to:", save_path)
+                print(Fore.BLUE + "OK - Video has been downloaded to:", save_path)
             elif choice == "2":
                 save_path_audio = os.path.join(script_dir, "audio")
                 download_audio_highest_quality(input_url, save_path_audio)
             else:
                 print("Invalid choice. Enter 1 for video or 2 for audio.")
-            again = input("Want more (y/n): ")
+            again = input(Fore.GREEN + "Want more (y/n): ")
             if again.lower() != "y":
                 break
         else:
-            print("Wrong link.")
+            print(Fore.RED + "Wrong link.")
